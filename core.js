9ba4351a2727f5596f769786421fabe676ac4dee
taehee kim <ktthee1995@gmail.com>
2021-08-27 18:06:08 +0900

    feat: added favicon

--- core.js
+++ core.js
@@ -1,15 +1,19 @@
-// number of people
-const NUM_PEOPLE = 5;
-// max sequential call of number
-const MAX_CALL = 3;
-// end number of the game
-const END_NUMBER = 31;
-// default absolute tolerance of arithematic comparison
-const ABS_TOL = 10e-6;
+const defaultGameSetting = {
+    endNumber: 31,
+    numPeople: 4,
+    maxCall: 3,
+};
+
+const defaultSimulationSetting = {
+    absTol: 10e-5
+};
+
+const defaultChooseStratgy = (loseRate) => chooseRateOpt(loseRate, defaultSimulationSetting);
 
 // choseProb([1, 0, 0]) = [0, 1/2, 1/2]
 // choseProb([1/2, 1/2, 0]) = [0, 0, 1]
-function chooseRateOpt(loseRate, absTol = ABS_TOL) {
+function chooseRateOpt(loseRate, simulationSetting) {
+    const { absTol } = simulationSetting;
     const lowest = loseRate.reduce((lowest, current) => lowest > current? current: lowest);
     const check = loseRate.map((x) => x - lowest < absTol? 1 : 0);
     const checkSum = check.reduce((accum, current) => accum + current);
@@ -26,32 +30,36 @@ function rotateArray(arr) {
     return result;
 }
 
-function _nextLoseProb(lookupMat, chooseStrategy=chooseRateOpt) {
+function _nextLoseProb(lookupMat, chooseStrategy) {
     const chooseRate = chooseStrategy(lookupMat.map((vector) => vector[0]));
     const result = lookupMat.reduce((accum, vector, rowIndex) => {
         const norm_vector = rotateArray(vector).map((value) => value*chooseRate[rowIndex]);
         return accum.map((value, index) => value + norm_vector[index]);
-    }, Array.from({length: NUM_PEOPLE}, ()=>0));
+    }, Array.from({length: lookupMat[0].length}, ()=>0));
     return result;
 }
 
 // lose rate after calling up to number.
+// chooseStrategy: (number, loseRate, )
 // loseProb(31) = [1, 0, 0]
 // loseProb(30) = [0, 1, 0]
-function loseProb(number, chooseStrategy=chooseRateOpt) {
-    if (number > END_NUMBER) {
+
+
+function loseProb(number, chooseStrategy, gameSetting=defaultGameSetting) {
+    const {endNumber, numPeople, maxCall} = gameSetting;
+    if (number > endNumber) {
         throw new Error("current number cannot exceed END_NUMBER");
     }
-    const initial = Array.from({length: NUM_PEOPLE}, () => 0);
+    const initial = Array.from({length: numPeople}, () => 0);
     initial[0] = 1;
 
     const loseMatrix = [initial];
-    while (loseMatrix.length < END_NUMBER - number) {
+    while (loseMatrix.length < endNumber - number) {
         // when calling row + 1
-        const lookupMat = loseMatrix.slice(loseMatrix.length-MAX_CALL+1, loseMatrix.length);
+        const lookupMat = loseMatrix.slice(loseMatrix.length-maxCall+1, loseMatrix.length);
         loseMatrix.push(_nextLoseProb(lookupMat, chooseStrategy));
     }
-    loseMatrix.forEach((val, ind) => console.log(END_NUMBER - ind, val));
+    loseMatrix.forEach((val, ind) => console.log(endNumber - ind, val));
     return loseMatrix[loseMatrix.length - 1];
 }
 
@@ -59,4 +67,4 @@ function winProb(n) {
     return loseProb(n).map(n=>1-n);
 }
 
-console.log(loseProb(-1));
+console.log(loseProb(-1, defaultChooseStratgy, defaultGameSetting));
